cfc219c8777e1b7eba691031adf8a22036702735
pjy1368 <basejin0810@naver.com>
2021-01-31 14:59:16 +0900

    문자열 내림차순으로 배치하기.java

---
+++ Level1/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String s) {
+        return Stream.of(s.split(""))
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.joining());
+    }
+}
