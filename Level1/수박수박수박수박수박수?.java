b04542ebe3ac18b6d89fb1b0bfc838febf79420c
JinYoung Park <basejin0810@naver.com>
2021-02-01 12:26:42 +0900

    수박수박수박수박수박수?.java

--- Level1/수박수박수박수박수박수?.java
+++ Level1/수박수박수박수박수박수?.java
@@ -1,5 +1,6 @@
 class Solution {
     public String solution(int n) {
+        // 수박 글자를 5,000개 쓴 데이터를 저장하고, substring() 함수를 이용하여 풀어도 무방함.
         return new String(new char[n / 2 + 1]).replace("\0", "수박").substring(0, n);
     }
 }
